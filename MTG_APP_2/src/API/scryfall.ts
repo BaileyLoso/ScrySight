b176977f86fefec065b92efcd48e2b1e39be9875
Bailey Loso <143433156+BaileyLoso@users.noreply.github.com>
2025-06-07 17:43:10 -0500

    Continue work on search and reorganize file structure.

--- MTG_APP_2/src/API/scryfall.ts
+++ MTG_APP_2/src/API/scryfall.ts
@@ -2,11 +2,10 @@
  /* Functions used to fetch data from the Scryfall API*/
 let url: string = 'https://api.scryfall.com';
 
-async function cardSearch(input: string): Promise<void> {
+export async function cardSearch(input: string): Promise<void> {
   try {
     const encodedInput = encodeURIComponent(input.trim());
     const formattedInput = encodedInput.replace(/%22/g, '"').replace(/%20/g, '+');
-
     console.log(formattedInput);
     const response = await fetch(`${url}/cards/search?q=${encodeURIComponent(formattedInput)}`);
     const data = await response.json();
--- MTG_APP_2/src/App.tsx
+++ MTG_APP_2/src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import viteLogo from './assets/vite.svg'
+//import { cardSearch } from './API/scryfall.js'
+import './styles/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Magic: The Gathering Card Search</h1>
+      <div id='card-search-bar'>
+        <input type="text" placeholder='Search...'></input>
+        <button id='search-submit-button'>Search</button>
+      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
